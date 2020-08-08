5de3f0102f09a05e553c98c653d379212d409697
Michael Cui <cuimichael500@gmail.com>
2020-08-08 00:11:02 -0500

    started working on db.js

---
+++ public/db.js
@@ -0,0 +1,16 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
